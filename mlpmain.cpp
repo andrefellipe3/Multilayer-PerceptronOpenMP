fd88aa788f55e2f0a343164d385bf3134bcafca5
Andre Fellipe <andrefellipe2008@hotmail.com>
2024-10-29 22:14:06 -0300

    a

--- mlpmain.cpp
+++ mlpmain.cpp
@@ -1,68 +1,74 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <time.h>
-#include <omp.h>
-// BIBLIOTECA COM CLASSES E FUNÇÕES DO MLP
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<time.h>
+#include<omp.h>
+//BIBLIOTECA COM CLASSES E FUNCOES DO MLP
 #include "mlp.hpp"
-#pragma omp num_threads(4)
+
 using namespace std;
+#pragma omp num_threads(4)
 
 int main()
 {
-    int i, j, qtTestCases = 0, qtTrainCases = 0;
-    float vec[inLength] = {0};
-    FILE *trainDataset, *testDataset;
-    mlp mlp; // classe contendo os pesos, biases, resultados, informações e funções do MLP
 
-    // Acessando arquivo contendo trainDataset
-    trainDataset = fopen(trainFile, "r");
-    if (trainDataset == NULL) {
-        printf("error opening trainDataset file\n");
-        return -1;
-    }
+	int i, j, k=0, qtTestCases=0, qtTrainCases=0;
+	char c;
+	float vec[inLength] = {0};
+	FILE *trainDataset, *testDataset;
+	mlp mlp; //classe contendo os pesos, biases, resultados, informacoes e funcoes do MLP
+	
+	//acessando arquivo contendo trainDataset
+	trainDataset = fopen(trainFile, "r");
+	if(trainDataset == NULL){
+		printf("error opening trainDataset file\n");
+		return -1;
+	}
 
-    // Contando quantidade de linhas do trainDataset = quantidade de "casos treino" para o MLP
-    qtTrainCases = countLines(trainDataset);
+	//contando quantidade de linhas do trainDataset = quantidade de "casos treino" para o MLP
+	qtTrainCases = countLines(trainDataset);
 
-    // Matrizes de entradas e de saídas para treinar o MLP
-    float X[qtTrainCases][inLength], Y[qtTrainCases][outLength];
+	//matrizes de entradas e de saidas para treinar o MLP
+	float X[qtTrainCases][inLength], Y[qtTrainCases][outLength];
 
-    // Preenchendo matrizes com dados do trainDataset
-    #pragma omp parallel for private(j) // Paralelização do loop sobre os casos de treino
-    for (i = 0; i < qtTrainCases; i++) {
-        for (j = 0; j < inLength; j++) {
-            fscanf(trainDataset, "%f", &X[i][j]);
-        }
-        for (j = 0; j < outLength; j++) {
-            fscanf(trainDataset, "%f", &Y[i][j]);
-        }
-    }
-    fclose(trainDataset);
+	//preenchendo matrizes com dados do trainDataset
+	#pragma omp paralell for
+	for(i=0;i<qtTrainCases;i++)
+	{
+		for(j=0;j<inLength;j++)
+		{
+			fscanf(trainDataset, "%f", &X[i][j]);
+		}
+		for(j=0;j<outLength;j++)
+		{
+			fscanf(trainDataset, "%f", &Y[i][j]);
+		}
+	}
+	fclose(trainDataset);
 
-    // Treinando MLP (a função backpropagation já está paralelizada internamente)
-    mlp.backpropagation(X, Y, qtTrainCases);
+	//treinando MLP
+	mlp.backpropagation(X, Y, qtTrainCases);
 
-    // Testando MLP
-    testDataset = fopen(testFile, "r");
-    if (testDataset == NULL) {
-        cout << "error opening testDataset file\n";
-        return -1;
-    }
+	//testando MLP
+	testDataset = fopen(testFile, "r");
+	if(testDataset == NULL){
+		cout << "error opening testDataset file\n";
+		return -1;
+	}
 
-    qtTestCases = countLines(testDataset);
+	qtTestCases = countLines(testDataset);
 
-    #pragma omp parallel for private(j, vec) // Paralelização do loop sobre os casos de teste
-    for (i = 0; i < qtTestCases; i++) {
-        for (j = 0; j < inLength; j++) {
-            fscanf(testDataset, "%f", &vec[j]);
-        }
-        mlp.forward(vec);
-        mlp.printResult();
-    }
-    fclose(testDataset);
+	for(i=0;i<qtTestCases;i++){
+		for(j=0;j<inLength;j++){
+			fscanf(testDataset, "%f", &vec[j]);
+		}
+		mlp.forward(vec);
+		mlp.printResult();
+	}
+	fclose(testDataset);
 
-    return 0;
+
+	return 0;
 }
